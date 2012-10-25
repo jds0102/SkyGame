44603ad0c7fcffeb9afe7932896b3d6725301cc4
hardikar <shreedharhardikar@gmail.com>
2012-10-24 21:21:48 -0500

    some shader done

--- Portal.js
+++ Portal.js
@@ -16,7 +16,9 @@ Portal = function (pos, rot) {
     this.init();  // init scene node specific members
     //this.Pos = pos;
     //this.Rot = rot;
-    this.size = 50;
+
+    this.size = 20;
+    this.Scale = new CL3D.Vect3d(this.size, this.size, this.size);
     this.initShader();
 
     // create a 3d mesh with one mesh buffer
@@ -26,17 +28,18 @@ Portal = function (pos, rot) {
 
     // set indices and verticesaa
     buf.Indices = [3, 1, 0, 2, 1, 3];
-    buf.Vertices.push(createVertex(50, 50, 0, 1,0));
-    buf.Vertices.push(createVertex(-50, 50, 0, 0,0));
-    buf.Vertices.push(createVertex(-50, -50, 0, 0,1));
-    buf.Vertices.push(createVertex(50, -50, 0, 1,1));
+    buf.Vertices.push(createVertex(1, 1, 0, 1, 0));
+    buf.Vertices.push(createVertex(-1, 1, 0, 0, 0));
+    buf.Vertices.push(createVertex(-1, -1, 0, 0, 1));
+    buf.Vertices.push(createVertex(1, -1, 0, 1, 1));
 
     buf.Mat = new CL3D.Material();
     //alert(buf.Mat.Type);
     buf.Mat.Type = this.portalShaderType;
     buf.Mat.BackFaceCulling = false;
-
+    buf.Mat.isTransparent = function () { return true; }
     this.mesh.AddMeshBuffer(buf);
+
 }
 
 Portal.prototype = new CL3D.SceneNode(); // derive from SceneNode
@@ -77,22 +80,44 @@ Portal.prototype.initShader = function () {
         precision highp float;          \n\
         #endif                          \n\
         uniform float radius;             \
+        uniform float nBands;           \
         varying vec2 uv;       \
         \
         void main()                     \
         {                               \
-        float nBands = 4.0;           \
-        float c = mod(uv.t, nBands);             \
-        if ( c < 1.0 ) \
-        gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);  \
+        float angle = 0.8;\
+        vec2 center = vec2(radius , radius );\
+        vec2 tc = uv * radius * 2.0; \
+        tc -= center; \
+        float dist = length(tc);\
+        if (dist > (radius) )\
+            discard;\
+        \
+        float percent = (radius - dist) / radius;\
+        float theta = percent * percent * angle * 8.0;\
+        float S = sin(theta); \
+        float C = cos(theta); \
+        tc = vec2(dot(tc, vec2(C, -S)), dot(tc, vec2(S, C)));   \
+        tc += center;\
+        tc = tc / radius;\
+        \
+        float u = tc.t * nBands;           \
+        u = mod(u, 2.0);             \
+        vec4 c;                   \
+        if ( u < 1.0 ) \
+        gl_FragColor  = vec4(0.0, 0.0, 0.5, 0.5);  \
         else \
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);  \
+        gl_FragColor  = vec4(0.0, 0.0, 1.0, 0.5);  \
+        if ( dist > radius * 0.95) \
+            gl_FragColor  = vec4(233.0 / 256.0, 214.0/ 256.0, 106.0 / 256.0, 0.5);  \
         }";
 
     renderer = engine.getRenderer();
-    this.portalShaderType = renderer.createMaterialType(vertex_shader_source, fragment_shader_source);
+    this.portalShaderType = renderer.createMaterialType(vertex_shader_source, fragment_shader_source, true, renderer.gl.ONE, renderer.gl.ONE_MINUS_SRC_ALPHA);
     shader = renderer.getGLProgramFromMaterialType(this.portalShaderType);
     shader.radius = renderer.gl.getUniformLocation(shader, "radius");
-    renderer.gl.uniform1f(shader.radius, this.size);
+    shader.nBands = renderer.gl.getUniformLocation(shader, "nBands");
+    renderer.gl.uniform1f(shader.radius, this.size/2);
+    renderer.gl.uniform1f(shader.nBands, 20.0);
 
 }
