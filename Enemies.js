3f7949480a5a41a929a3cdedda00441f52186761
Mik Xin <mikxintan@Miks-MacBook-Pro.local>
2012-10-26 11:37:34 -0500

    enemy simple shader... x)

--- Enemies.js
+++ Enemies.js
@@ -11,6 +11,10 @@ function EnemyTower(name) {
     this.rotSpeed = 0.1;
     this.direction = new CL3D.Vect3d(0, 0, 1);
     this.init_direction = self.direction.clone();
+    
+    this.dFrame = 0.1;
+    this.frame = 0;
+    var shader1;
 
     this.isShooting = true;
 
@@ -47,6 +51,20 @@ function EnemyTower(name) {
     }
 
     this.update = function () {
+         if (self.frame >= 60 || self.frame <= 0)
+             self.dFrame = -self.dFrame;
+        
+ 		//console.log(self.frame);
+ 		
+ 		self.frame = self.frame + self.dFrame;
+        shader1 = renderer.getGLProgramFromMaterialType(newMaterialType);
+        shader1.time = renderer.gl.getUniformLocation(shader1, "time");
+        renderer.gl.uniform1f(shader1.time, self.frame);
+    
+        newMaterialType = renderer.createMaterialType(vertex_shader_source, fragment_shader_source);
+        
+ 		self.node.getMaterial(4).Type = newMaterialType;   
+        
         self.node.Rot.Y = (self.node.Rot.Y + self.rotSpeed) % 360;
         self.updateDirection();
         if (self.node.Rot.Y < self.rotRange.min || self.node.Rot.Y > self.rotRange.max)
@@ -58,4 +76,48 @@ function EnemyTower(name) {
     this.pulseHit = function () {
 
     }
+    var vertex_shader_source = 
+                "\
+        		   #ifdef GL_ES                    \n\
+        		   precision highp float;          \n\
+        		   #endif                          \n\
+        		   uniform mat4 worldviewproj;     \
+        		   uniform float time; \
+        		   attribute vec4 vPosition;       \
+        		   varying float intensity;     \
+        		   uniform vec3 lightDir;       \
+        		   attribute vec3 vNormal;         \
+		           attribute vec2 vTexCoord1;      \
+		           varying vec2 uv;       \
+        		   void main()                     \
+        		   {      \
+        		   vec4 newPos = vPosition + vec4(vNormal, 1.0) * time; \
+                    intensity = dot(lightDir,vNormal);         \
+                    gl_Position = worldviewproj * newPos ; \
+        		   }";
+        		   
+        var fragment_shader_source = 
+        "\
+        #ifdef GL_ES                    \n\
+        precision highp float;          \n\
+        #endif                          \n\
+        uniform float radius;            \
+        uniform float time; \
+        varying float intensity;             \
+        varying vec2 uv;       \
+        \
+        void main()                     \
+        {                               \
+	    gl_FragColor = vec4(1.0,0.41,0.15,1.0); \
+        }";
+        
+        
+    
+        renderer = engine.getRenderer();
+        newMaterialType = renderer.createMaterialType(vertex_shader_source, fragment_shader_source);
+        shader1 = renderer.getGLProgramFromMaterialType(newMaterialType);
+        shader1.time = renderer.gl.getUniformLocation(shader1, "time");
+        renderer.gl.uniform1f(shader1.time, self.frame);
+        
+        
 }
